83c40c9043a676166bf5741707e50c9557abcb04
Biniam <biniam22@gmail.com>
2018-03-26 23:50:27 -0400

    javaScript assignment

---
+++ Range Print/range.js
@@ -0,0 +1,11 @@
+var skip = 2;
+        function printRange()
+        {
+          for (var start = 0; start < 8; start=start+2 ){
+          start= start + skip;
+          console.log(start);
+          }
+          
+          }
+         
+        printRange();
