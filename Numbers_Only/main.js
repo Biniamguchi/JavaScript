c603a444116083c6f704af72bfcbcbe31b2985c6
Biniam <biniam22@gmail.com>
2019-05-30 03:12:06 -0400

    Numbers only

--- Numbers_Only/main.js
+++ Numbers_Only/main.js
@@ -8,7 +8,7 @@ function numOnly(arr){
         }
     }
 
-    return newArr;
+    return newArr; 
 }
 
 function remNums(arr){
@@ -27,3 +27,15 @@ console.log(cp);
 remNums(cp);
 console.log(cp);
 
+// function numOnly(arr){
+//     var newArr = [];
+//     var arr = ["hello",7,768,"test",48,"world",123,77];
+//     for(let i=0;i<arr.length;i++){
+        
+//         if(typeof(arr[i]) === "number"){
+//             newArr.push(arr[i]);
+//         }
+//     }
+
+//   console.log(newArr);  
+// }numOnly();
