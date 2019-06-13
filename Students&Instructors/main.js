1cd1340c98dd788f5d2d4471c64a318cf79a7627
Biniam <biniam22@gmail.com>
2019-06-13 00:20:35 -0400

    Students&Instructors

--- Students&Instructors/main.js
+++ Students&Instructors/main.js
@@ -23,24 +23,24 @@ var users = {
   ]
  };
 
-for(var name in users){
-    console.log(name);
+// for(var name in users){
+//     console.log(name);
 
-for(var i in users[name]){
-    var numLength = users[name][i].first_name.length+users[name][i].last_name.length;
-    console.log(i+" -",users[name][i].first_name,users[name][i].last_name,"- "+numLength);
-  }
+// for(var i in users[name]){
+//     var numLength = users[name][i].first_name.length+users[name][i].last_name.length;
+//     console.log(i+" -",users[name][i].first_name,users[name][i].last_name,"- "+numLength);
+//   }
 
-}
-
-// for(i in users){
-//     var j = users[i];
-
-//     console.log(i);
-
-//     for(k in j){
-//         var l = j[k];
-//         var nmLen = l.firstName.length+l.lastName.length;
-//         console.log(k+" -",l.firstName,l.lastName,"- "+nmLen);
-//     }
 // }
+
+for(var i in users){
+    var j = users[i];
+ 
+    console.log(i);
+
+    for(var k in j){
+        var l = j[k];
+        var nmLen = l.first_name.length+l.last_name.length;
+        console.log(k+" -",l.first_name,l.last_name,"- "+nmLen);
+    }
+}
